e1444ae9d5ed716bacda1c6dbe4e9545f13757e3
Catalino Cuadrado <catalino.cuadrado@nasa.gov>
2014-01-07 09:53:34 -0500

    Testing a new script implementation

--- spec/test_runner.js
+++ spec/test_runner.js
@@ -1,20 +1,34 @@
-console.log('Loading a web page');
-var page = new WebPage();
-//This was tricky, this is the way to open LOCAL files
-var url = "file://SpecRunner.html";
-phantom.viewportSize = {width: 800, height: 600};
-//This is required because PhantomJS sandboxes the website and it does not show up the console messages form that page by default
-page.onConsoleMessage = function (msg) { console.log(msg); };
-//Open the website
-page.open(url, function (status) {
-    //Page is loaded!
+(function () {
+    "use strict";
+    var system = require("system");
+    var url = system.args[1];
+ 
+    phantom.viewportSize = {width: 800, height: 600};
+ 
+    console.log("Opening " + url);
+ 
+    var page = new WebPage();
+ 
+    // This is required because PhantomJS sandboxes the website and it does not
+    // show up the console messages form that page by default
+    page.onConsoleMessage = function (msg) {
+        console.log(msg);
+ 
+        // Exit as soon as the last test finishes.
+        if (msg && msg.indexOf("Dixi.") !== -1) {
+            phantom.exit();
+        }
+    };
+ 
+    page.open(url, function (status) {
         if (status !== 'success') {
-                console.log('Unable to load the address!');
-            } else {
-                 //Using a delay to make sure the JavaScript is executed in the browser
-               window.setTimeout(function () {
-                    page.render("output.png");
-                    phantom.exit();
-               }, 200);
-                }
-});
+            console.log('Unable to load the address!');
+            phantom.exit(-1);
+        } else {
+            // Timeout - kill PhantomJS if still not done after 2 minutes.
+            window.setTimeout(function () {
+                phantom.exit();
+            }, 120 * 1000); // NB: use accurately, tune up referring to your needs
+        }
+    });
+}());
