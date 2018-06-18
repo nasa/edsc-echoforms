dc20c28abc3ca7f89359c4aa9eb35f3635621a8d
Ryan Abbott <ryan@element84.com>
2018-06-18 14:10:39 -0400

    EDSC-1885: Updates dependencies

--- src/bundle-full.js
+++ src/bundle-full.js
@@ -1,7 +1,7 @@
 require('../node_modules/jstree/dist/themes/default/style.min.css');
 require("./echoforms.less");
-require('script!../node_modules/wicked-good-xpath/dist/wgxpath.install.js');
-require('script!../node_modules/simple-slider/js/simple-slider.min.js');
-require('script!../node_modules/jstree/dist/jstree.min.js');
+require('script-loader!../node_modules/wicked-good-xpath/dist/wgxpath.install.js');
+require('script-loader!../node_modules/simple-slider/js/simple-slider.min.js');
+require('script-loader!../node_modules/jstree/dist/jstree.min.js');
 require("./index.coffee");
 require("./extras.js");
--- src/bundle.js
+++ src/bundle.js
@@ -1,3 +1,3 @@
-require('expose?wgxpath!../node_modules/wicked-good-xpath/dist/wgxpath.install.js');
+require('expose-loader?wgxpath!../node_modules/wicked-good-xpath/dist/wgxpath.install.js');
 require("./echoforms.less");
 require("./index.coffee");
