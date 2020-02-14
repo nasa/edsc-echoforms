bf6dab7e5e701ea8d72b70dcbc8cb7e6319b3b64
Matthew Crouch <matthew@element84.com>
2020-02-14 11:23:51 -0500

    EDSC-2594: Remove dependency on react-bootstrap, made bootstrap classes optional

--- src/components/TextField/__tests__/TextField.test.js
+++ src/components/TextField/__tests__/TextField.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { Form } from 'react-bootstrap'
 
 import { TextField } from '../TextField'
 
@@ -29,13 +28,13 @@ describe('TextField component', () => {
   test('renders a Form.Check component', () => {
     const { enzymeWrapper } = setup()
 
-    expect(enzymeWrapper.find(Form.Control).length).toBe(1)
+    expect(enzymeWrapper.find('input').length).toBe(1)
   })
 
   test('onChange calls onUpdateModel', () => {
     const { enzymeWrapper, props } = setup()
 
-    const TextField = enzymeWrapper.find(Form.Control)
+    const TextField = enzymeWrapper.find('input')
 
     TextField.props().onChange({ target: { value: 'new test value' } })
 
