8e251cff8db564c9db36758a4028090c5049ee7b
Matthew Crouch <matthew@element84.com>
2020-06-12 18:26:54 -0400

    EDSC-2686: Fix absolute xpaths

--- src/util/evaluateXpath.js
+++ src/util/evaluateXpath.js
@@ -4,20 +4,31 @@
  * @param {Object} model
  */
 export const evaluateXpath = (xpath, model, resolver) => {
+  let updatedXpath = xpath
   if (xpath.charAt(0) === '[') {
-    // eslint-disable-next-line no-param-reassign
-    xpath = `self::*${xpath}`
+    updatedXpath = `self::*${xpath}`
   }
+
+  // The root of the document being referred to is actually <form>,
+  // not <model>, so absolute xpaths will not work as expected.
+  // so we need to replace '/' at the beginning of the path, as well as '//' anywhere with:
+  // descendant-or-self::node()
+  updatedXpath = updatedXpath.replace(/^\/+|(https:)?\/\//g, ($0, $1) => {
+    if ($1) { return $0 } return '/descendant-or-self::node()/'
+  })
+
+  const doc = model.ownerDocument
 
   let modelForEvaluation = model
 
   // If absolute xpath is provided, evaluate the xpath based on the ownerDocument of the model.
-  if (xpath.startsWith('//')) {
-    modelForEvaluation = model.ownerDocument
+  if (xpath.startsWith('/')) {
+    const modelResult = doc.evaluate('//*[local-name()="instance"]/*', doc, resolver, XPathResult.ANY_TYPE, null)
+    modelForEvaluation = modelResult.iterateNext()
   }
 
-  const result = model.ownerDocument.evaluate(
-    xpath,
+  const result = doc.evaluate(
+    updatedXpath,
     modelForEvaluation,
     resolver,
     XPathResult.ANY_TYPE,
