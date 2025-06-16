f242a646ac78918e826ae763240b13872853a9d5
KushaalP <kpalasamudrum@gmail.com>
2025-06-16 11:31:51 -0400

    EDSC: fixed  comment issue

--- src/components/FormBody/FormBody.jsx
+++ src/components/FormBody/FormBody.jsx
@@ -15,7 +15,7 @@ export const FormBody = ({
 
   /**
    * Prevents form submission when Enter is pressed on any form element
-   * @param {Object} e event object
+   * @param {Object} keyPressEvent event object
    */
   const onKeyDown = (keyPressEvent) => {
     if (keyPressEvent.key === 'Enter') {
